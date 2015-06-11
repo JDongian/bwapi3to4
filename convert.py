ad240ff7ee2ce621e4532a680e55511aaa241395
Bjorn P Mattsson <bjorn.pm@plankter.se>
2015-06-11 15:19:07 +0200

    Specify bot using command line, bot_sources in gitignore

--- convert.py
+++ convert.py
@@ -10,7 +10,7 @@ DEFAULT_BOT = "ualberta"
 def get_source_files(bot_name):
     source_files = []
 
-    for root, dirs, files in os.walk("./{}/{}".format(BOT_DIR, DEFAULT_BOT)):
+    for root, dirs, files in os.walk("./{}/{}".format(BOT_DIR, bot_name)):
         for f in files:
             if f.endswith(".cpp"):
                 source_files.append(os.path.join(root, f))
@@ -24,9 +24,9 @@ def convert(source):
 
 if __name__ == "__main__":
     bot = DEFAULT_BOT
-    #bot = input("Bot to convert: ")
+    bot = input("Bot to convert: ")
 
-    files = get_source_files(DEFAULT_BOT)
+    files = get_source_files(bot)
 
     for filename in files[:2]:
         old = None
